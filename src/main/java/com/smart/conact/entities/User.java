3487b4fb1f4812e42c3082384dfd175c80374c94
Gaurav Singh <grvsgh7@gmail.com>
2021-06-20 19:34:35 +0530

    Updated User table name

--- src/main/java/com/smart/conact/entities/User.java
+++ src/main/java/com/smart/conact/entities/User.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="USER")
+@Table(name="TBL_USER")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
